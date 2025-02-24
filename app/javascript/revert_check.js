b518a725814fea08b30c266b649bbaa5d3358c66
tomotomotom <torimorinorikeyhome@gmail.com>
2025-02-24 12:24:27 +0900

    確認用ポップアップの表示

--- app/javascript/revert_check.js
+++ app/javascript/revert_check.js
@@ -2,12 +2,17 @@ window.addEventListener('turbo:load', function(){
   const articleCheckButtons = document.querySelectorAll(".article-check-button");
   articleCheckButtons.forEach(function (button) {
     button.addEventListener("click", () => {
-      const articleId = button.getAttribute("data");
-      const XHR = new XMLHttpRequest();
-      XHR.open("GET", `/checks/revert/${articleId}`, true);
-      XHR.send();
-      XHR.onload = () => {
-        button.remove()
+      const deleteFlg = window.confirm('記事の既読を削除しますか？');
+      if(deleteFlg) {
+        const articleId = button.getAttribute("data");
+        const XHR = new XMLHttpRequest();
+        XHR.open("GET", `/checks/revert/${articleId}`, true);
+        XHR.send();
+        XHR.onload = () => {
+          button.remove();
+        }
+      } else {
+        ;
       }
     });
   });
